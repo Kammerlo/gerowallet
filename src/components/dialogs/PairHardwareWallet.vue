9573b1e3a9fe7da149a8b395b65301fb5393b1b3
Dudi Edri <dudi@anyclip.com>
2024-04-09 09:20:51 +0300

    First

--- src/components/dialogs/PairHardwareWallet.vue
+++ src/components/dialogs/PairHardwareWallet.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-dialog content-class="rounded-xxl dialogStyle" v-model="dialogLocal" scrollable max-width="850" :persistent="persistent">
+  <v-dialog content-class="rounded-xxl dialogStyle" v-model="dialogLocal" scrollable max-width="850">
     <v-card
         class="py-0 rounded-xxl transparent fill-height"
     >
@@ -43,61 +43,71 @@
                   >
                     <v-col>
                         <v-card flat class="fill-height transparent">
-                          <v-container
-                              style="width:80%;"
-                              class="justify-center text-center"
+                          <v-alert
+                              color="primary"
+                              dense
+                              outlined
+                              type="info"
+                              prominent
+                              border="left"
                           >
-                            <v-card-title class="justify-center pb-0" style="font-weight: 700;">
-                              Connect your wallet
-                            </v-card-title>
-                            <v-card-text class="text-center">
-                              Connect with one of our available wallet providers or install a new one.
-                            </v-card-text>
-                            <v-item-group v-model="walletType">
-                            <v-row no-gutters>
-                              <v-col
-                                  v-for="(item) in walletTypes"
-                                  :key="item.name"
-                                  cols="12"
-                                  sm="6"
-                                  xs="12"
-                                  class="pa-2"
-                              >
-                                <v-item v-slot="{ active, toggle }">
-                                <v-hover>
-                                  <template v-slot:default="{ hover }">
-                                    <v-card
-                                        flat
-                                        height="150"
-                                        class="justify-center text-center pa-4 shadow"
-                                        :style="$vuetify.theme.isDark ? { backgroundColor: '#00000080', alignContent: 'center' } : { backgroundColor: '#ffffff80', alignContent: 'center'}"
-                                        @click="toggle"
-                                    >
-                                      <img
-                                          :src="$vuetify.theme.isDark ? item.icon : item.dark"
-                                          style="margin: auto; width: 130px; filter: invert(100%) sepia(20%) saturate(2%) hue-rotate(213deg) brightness(112%) contrast(101%);"
-                                          :alt="item.name"
-                                      />
-                                      <v-fade-transition>
-                                        <v-icon color="primary" style="position: absolute; right: 10px; bottom: 10px;" v-if="active">
-                                          mdi-check-circle-outline
-                                        </v-icon>
-                                      </v-fade-transition>
-                                        <v-overlay
-                                            v-if="hover"
-                                            absolute
-                                            color="#ffffff"
+                            Hardware wallets, a type of cold wallet, provide one of the most secure ways to keep cryptocurrencies. They work by storing your private keys in an external, physical device (usually a USB or Bluetooth device)
+                          </v-alert>
+                          <v-card-title class="justify-center" style="font-weight: 700; word-break: break-word">
+                            What Type of Hardware Wallet Would You Like to Connect With?
+                          </v-card-title>
+                          <v-card-text class="text-center">
+                            <v-item-group v-model="walletType" active-class="primary" class="pb-10">
+                              <v-row no-gutters>
+                                <v-col
+                                    v-for="(item) in walletTypes"
+                                    :key="item.name"
+                                    cols="12"
+                                    sm="6"
+                                    xs="12"
+                                    class="pa-2"
+                                >
+                                  <v-item v-slot="{ active, toggle }">
+                                    <v-hover>
+                                      <template v-slot:default="{ hover }">
+                                        <v-card
+                                            flat
+                                            height="150"
+                                            class="justify-center text-center pa-4 shadow"
+                                            :style="$vuetify.theme.isDark ? { backgroundColor: '#00000080', alignContent: 'center' } : { backgroundColor: '#ffffff80', alignContent: 'center'}"
+                                            @click="toggle"
                                         >
-                                        </v-overlay>
+                                          <div style="height: 90px; align-content: center;" >
+                                            <img
+                                                :src="$vuetify.theme.isDark ? item.icon : item.dark"
+                                                style="margin: auto; width: 130px; filter: invert(100%) sepia(20%) saturate(2%) hue-rotate(213deg) brightness(112%) contrast(101%);"
+                                                :alt="item.name"
+                                            />
+                                          </div>
 
-                                    </v-card>
-                                  </template>
-                                </v-hover>
-                                </v-item>
-                              </v-col>
-                            </v-row>
+                                          <v-card-subtitle class="pa-0">
+                                            {{ item.support }}
+                                          </v-card-subtitle>
+                                          <v-scroll-y-transition>
+                                            <v-icon color="white" style="position: absolute; right: 10px; bottom: 10px;" v-if="active">
+                                              mdi-check-circle-outline
+                                            </v-icon>
+                                          </v-scroll-y-transition>
+                                          <v-overlay
+                                              v-if="hover"
+                                              absolute
+                                              color="#ffffff"
+                                          >
+                                          </v-overlay>
+
+                                        </v-card>
+                                      </template>
+                                    </v-hover>
+                                  </v-item>
+                                </v-col>
+                              </v-row>
                             </v-item-group>
-                          </v-container>
+                          </v-card-text>
                         </v-card>
                     </v-col>
                   </v-row>
@@ -118,66 +128,22 @@
           </v-stepper-content>
 
           <v-stepper-content step="2" style="text-align: -webkit-center;" class="pt-0">
-            <v-form ref="form2" v-model="valid2">
-              <v-card flat class="transparent" style="max-width: 526px; min-height: 591px">
-                <v-card-text class="px-0">
-                  <v-alert
-                      color="primary"
-                      dense
-                      outlined
-                      type="info"
-                      prominent
-                      border="left"
-                  >
-                    Write down or copy these words in the following order.<br>You will need
-                    these to back up and restore your wallet.
-                  </v-alert>
-                  <v-hover v-slot="{ hover }">
-                    <v-card flat outlined class="mb-4"
-                            :style="overlay ? {backgroundColor: 'black'} : {backgroundColor: 'transparent'}">
-                      <v-row no-gutters>
-                        <v-col class="pa-2" cols="12" md="3" v-for="(item,index) in seedPhrase" :key="index">
-                          <v-chip color="#5553" large style="width: 100%; height: 30px"><span
-                              style="color: #2f9cac">{{ (index + 1) + '.' }}</span>&nbsp;&nbsp;{{ item }}
-                          </v-chip>
-                        </v-col>
-                      </v-row>
-                      <v-overlay
-                          :absolute="true"
-                          :opacity="hover ? 0.4 : 1"
-                          :style=" hover && overlay ? { backdropFilter: 'blur(6px)' } : {}"
-                          color="black"
-                          :value="overlay"
-                      >
-                        <v-btn
-                            color="primary"
-                            @click="overlay = false"
-                        >
-                          <v-icon>mdi-key</v-icon>&nbsp;
-                          Unlock
-                        </v-btn>
-                      </v-overlay>
-                    </v-card>
-                  </v-hover>
-                  <v-alert
-                      dense
-                      type="error"
-                      prominent
-                      border="left"
-                      class="text-left"
-                  >
-                    Save the seed phrase somewhere safe and never share it with anyone.
-                  </v-alert>
-                  <v-checkbox
-                      class="mt-0"
-                      hide-details
-                      v-model="newWallet.recoverSeedChecked"
-                      label="I understand that if I lose my secret backup phrase, I will not be able to access my funds."
-                      :rules="[(newWallet.recoverSeedChecked)]"
-                  >
-                  </v-checkbox>
+            <v-form ref="form" v-model="valid2" style="padding-top: 12px; padding-bottom: 12px">
+              <v-card flat class="transparent d-flex row fill-height" style="max-width: 526px; min-height: 591px">
+                <v-card-text class="px-0 d-flex row justify-space-around mt-2">
+                  <img :src="require('@/assets/connect_ledger.svg')" alt="Connect Ledger">
                 </v-card-text>
-                <v-card-actions class="px-0 pt-4">
+                  <v-card-title class="justify-center text-center">
+                    Connect Ledger
+                  </v-card-title>
+                <v-card-text class="px-0 d-flex row justify-space-around mt-2">
+                  <ul class="text-left">
+                    <li>Connect the hardware wallet to your computer</li>
+                    <li>Unlock the hardware wallet by entering your pin code on the device</li>
+                    <li>Open the Cardano app on the hardware wallet</li>
+                  </ul>
+                </v-card-text>
+                <v-card-actions class="px-0 align-self-end" style="width: 100%">
                   <v-spacer></v-spacer>
                   <v-btn
                       color="primary"
@@ -319,7 +285,6 @@ export default {
         this.seedPhrase = bip39.generateMnemonic(256).split(" ");
         [this.seedPhraseToConfirm, this.seedPhraseReplaced] = this.randomReplace(this.seedPhrase, 4);
         this.step = 2
-        this.persistent = true
       }
     },
     walletCreationStep2() {
@@ -388,7 +353,6 @@ export default {
       this.seedPhrase = undefined
       this.seedPhraseToConfirm = undefined
       this.seedPhraseReplaced = undefined
-      this.persistent = false
     }
   },
   data: () => ({
@@ -413,10 +377,9 @@ export default {
     seedPhraseReplaced: undefined,
     overlay: true,
     opacity: 0.8,
-    persistent: false,
     walletTypes: [
-      {name: 'Ledger', description: 'The Ledger cryptocurrency hardware wallet made by Ledger, a company headquartered in Paris, France.', enabled: true, icon: require('@/assets/ledger.svg')},
-      {name: 'Trezor', description: 'Trezor comes from SatoshiLabs, based in the Czech Republic.', enabled: true, icon: require('@/assets/trezor.svg')},
+      {name: 'Ledger', description: 'The Ledger cryptocurrency hardware wallet made by Ledger, a company headquartered in Paris, France.', enabled: true, icon: require('@/assets/ledger.svg'), support: 'Nano S, Nano S Plus, Nano X'},
+      {name: 'Trezor', description: 'Trezor comes from SatoshiLabs, based in the Czech Republic.', enabled: true, icon: require('@/assets/trezor.svg'), support: 'Model T, Safe 3' },
     ],
     walletType: undefined
   })
