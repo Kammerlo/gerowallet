5de238644816e9f6a48954aea1eb95cab22eec4f
dudiedri <dudi.edri@ox.security>
2025-04-21 22:20:09 -0700

    Refactor: Comment out unused transaction handling code
    Removed references to unused functions and variables related to transaction handling in the background script. This is part of cleanup to streamline the code and avoid unnecessary processing. No functional changes were introduced with this update.

--- src/chrome/background.ts
+++ src/chrome/background.ts
@@ -23,13 +23,13 @@ import {
   getDrepKey,
   urlScan,
   getUnusedAddresses,
-  convertToTxSchema,
+  // convertToTxSchema,
 } from '@/chrome/serialization';
 import { ERROR } from '@/models/types';
 import Tab = chrome.tabs.Tab;
 import networks from '../shared/utils/networks';
 import { getDomain } from 'tldts';
-import { setAccountTransactions } from '@/chrome/backgroundWalletDB';
+// import { setAccountTransactions } from '@/chrome/backgroundWalletDB';
 
 if (import.meta.hot) {
   // @ts-expect-error for background HMR
@@ -605,11 +605,11 @@ app.add(METHOD.submitTx, async (request, sendResponse) => {
             throw APIError.InvalidRequest;
         }
       }
-      const utxos = await getStorage(STORAGE.utxos);
-      const txCbor = request.data.tx
+      // const utxos = await getStorage(STORAGE.utxos);
+      // const txCbor = request.data.tx
       const txId = await response.text();
-      const tx = convertToTxSchema(txId, txCbor, utxos, networks.resolveNetworkId(loggedWallet['chain'], loggedWallet['network']))
-      await setAccountTransactions(loggedWallet.id, [tx])
+      // const tx = convertToTxSchema(txId, txCbor, utxos, networks.resolveNetworkId(loggedWallet['chain'], loggedWallet['network']))
+      // await setAccountTransactions(loggedWallet.id, [tx])
       sendResponse({
         id: request.id,
         data: txId,
