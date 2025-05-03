286bef77a6f1fa7b2494ea153b9e005d2ff2e0a6
KyrSmaw <kirill86@gmail.com>
2025-05-02 21:39:44 -0400

    comment

--- src/api/api.ts
+++ src/api/api.ts
@@ -444,6 +444,7 @@ export class Api {
     },
 
     transactions: {
+      // this would called as Wallet.api.multisig.transactions.create()
       /**
        * Create a transaction
        * @param body - The body of the transaction to create
