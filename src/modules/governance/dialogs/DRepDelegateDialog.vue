7b2428811c18cf3784b5e87e92b80bf53e6fc87f
ArtemNuss <you@example.com>
2025-10-21 14:29:08 +0200

    Enhance CardanoGovernance and DRepDelegateDialog components for improved delegation handling
    - Added logic to display pending transaction status in CardanoGovernance.vue, enhancing user feedback during delegation.
    - Refactored DRepDelegateDialog.vue for better readability and consistency, including improved conditional rendering and formatting.
    - Updated computed properties to streamline transaction fee calculations and ensure accurate display of delegation information.
    - Cleaned up template syntax for better maintainability and user experience.

--- src/modules/governance/dialogs/DRepDelegateDialog.vue
+++ src/modules/governance/dialogs/DRepDelegateDialog.vue
@@ -6,17 +6,14 @@
     :loading="loading"
     :min-height="660"
     :width="700"
-    :subtitle="`Delegating your ${networks.resolveCurrencyTicker(loggedWallet?.chain, loggedWallet?.network)} to a Delegated Representative.`"
+    :subtitle="`Delegating your ${networks.resolveCurrencyTicker(
+      loggedWallet?.chain,
+      loggedWallet?.network
+    )} to a Delegated Representative.`"
     :persistent="false"
   >
     <v-card-text class="px-3 justify-center text-center" style="z-index: 1" v-if="drep">
-      <v-alert
-        border="left"
-        color="primary"
-        type="info"
-        prominent
-        class="text-left"
-      >
+      <v-alert border="left" color="primary" type="info" prominent class="text-left">
         <ul>
           <li>You can only delegate to one DRep at a time</li>
           <li>You can switch to delegate to a different DRep at any time</li>
@@ -28,69 +25,108 @@
           <v-list-item-title class="text-h5 mb-1">
             {{ `${drep.name}` }}
           </v-list-item-title>
-          <v-list-item-subtitle v-if="drep">{{ truncate(drep.id) }}<CopyButton class="ml-1" :value="drep.id" x-small></CopyButton></v-list-item-subtitle>
+          <v-list-item-subtitle v-if="drep"
+            >{{ truncate(drep.id) }}<CopyButton class="ml-1" v-if="drep.id" :value="drep.id" x-small></CopyButton
+          ></v-list-item-subtitle>
           <v-list-item-subtitle v-if="drep.links">
             <template v-for="(link, index) in drep.links">
-              <v-btn icon x-small  :key="index" :href="link.uri" target="_blank" v-if="link.uri && typeof link.uri === 'string'">
-                <v-avatar tile size="14" v-if="String(link.uri).includes('https://x.com') || String(link.uri).includes('https://twitter.com')">
+              <v-btn
+                icon
+                x-small
+                :key="index"
+                :href="link.uri"
+                target="_blank"
+                v-if="link.uri && typeof link.uri === 'string'"
+              >
+                <v-avatar
+                  tile
+                  size="14"
+                  v-if="String(link.uri).includes('https://x.com') || String(link.uri).includes('https://twitter.com')"
+                >
                   <v-img :src="xLogo" alt="x"></v-img>
                 </v-avatar>
                 <v-avatar tile size="14" v-else-if="String(link.uri).includes('https://t.me')">
                   <v-img :src="telegramLogo" alt="x"></v-img>
                 </v-avatar>
                 <v-icon v-else>
-                  {{ getIconByURI(link.uri)}}
+                  {{ getIconByURI(link.uri) }}
                 </v-icon>
               </v-btn>
             </template>
           </v-list-item-subtitle>
         </v-list-item-content>
 
-        <v-list-item-avatar
-          rounded
-          size="80"
-          v-if="drep.image"
-        >
-          <img :src="drep.image" alt="" @error="fallbackImage"/>
+        <v-list-item-avatar rounded size="80" v-if="drep.image">
+          <img :src="drep.image" alt="" @error="fallbackImage" />
         </v-list-item-avatar>
       </v-list-item>
-      <v-card-title class="pt-0" style="color: white">{{ drep.delegators }}</v-card-title>
-      <v-card-subtitle class="text-left pb-2">Delegators</v-card-subtitle>
-      <v-card-title class="pt-0" style="color: white">{{ drep.votes }}</v-card-title>
-      <v-card-subtitle class="text-left pb-2">Votes</v-card-subtitle>
-      <v-card-title class="pt-0" style="color: white">{{ toCurrency(drep.voting_power, false, 0, networks.resolveCurrencySymbol(loggedWallet?.chain, loggedWallet?.network))}}</v-card-title>
-      <v-card-subtitle class="text-left pb-2">Voting Power</v-card-subtitle>
+      <v-card-title class="pt-0" style="color: white" v-if="drep.delegators">{{ drep.delegators }}</v-card-title>
+      <v-card-subtitle class="text-left pb-2" v-if="drep.delegators">Delegators</v-card-subtitle>
+      <v-card-title class="pt-0" style="color: white" v-if="drep.votes">{{ drep.votes }}</v-card-title>
+      <v-card-subtitle class="text-left pb-2" v-if="drep.votes">Votes</v-card-subtitle>
+      <v-card-title class="pt-0" style="color: white" v-if="drep.voting_power">{{
+        toCurrency(
+          drep.voting_power,
+          false,
+          0,
+          networks.resolveCurrencySymbol(loggedWallet?.chain, loggedWallet?.network)
+        )
+      }}</v-card-title>
+      <v-card-subtitle class="text-left pb-2" v-if="drep.voting_power">Voting Power</v-card-subtitle>
     </v-card-text>
-    <v-card-actions class="justify-center text-center pt-0 px-3" v-if="drep && account" style="display: block;">
+    <v-card-actions class="justify-center text-center pt-0 px-3" v-if="drep && account" style="display: block">
       <v-form ref="form" v-model="valid">
         <v-row no-gutters>
           <v-col :cols="cols">
-            <h4>Delegation Amt.
+            <h4>
+              Delegation Amt.
               <v-btn x-small icon>
                 <v-icon small>mdi-information-outline</v-icon>
               </v-btn>
             </h4>
-            <h4><strong>{{ toCurrency(account.controlled_amount, false, 0, networks.resolveCurrencySymbol(loggedWallet?.chain, loggedWallet?.network))}}</strong></h4>
+            <h4>
+              <strong>{{
+                toCurrency(
+                  account.controlled_amount,
+                  false,
+                  0,
+                  networks.resolveCurrencySymbol(loggedWallet?.chain, loggedWallet?.network)
+                )
+              }}</strong>
+            </h4>
           </v-col>
           <v-col :cols="cols" v-if="depositFee > 0">
             <h4>Deposit Fee</h4>
-            <h4><strong>{{ toCurrency(depositFee, false, 0, networks.resolveCurrencySymbol(loggedWallet?.chain, loggedWallet?.network)) }}</strong></h4>
+            <h4>
+              <strong>{{
+                toCurrency(
+                  depositFee,
+                  false,
+                  0,
+                  networks.resolveCurrencySymbol(loggedWallet?.chain, loggedWallet?.network)
+                )
+              }}</strong>
+            </h4>
           </v-col>
           <v-col :cols="cols">
             <h4>Tx Fee</h4>
-            <h4><strong>{{ toCurrency(tx?.body?.fee?.toString() || '0', false, 0, networks.resolveCurrencySymbol(loggedWallet?.chain, loggedWallet?.network)) }}</strong></h4>
+            <h4>
+              <strong>{{
+                toCurrency(
+                  tx?.body?.fee?.toString() || '0',
+                  false,
+                  0,
+                  networks.resolveCurrencySymbol(loggedWallet?.chain, loggedWallet?.network)
+                )
+              }}</strong>
+            </h4>
           </v-col>
-          <v-col cols="12" class="pt-6" style="display: flex; justify-content: center;">
-            <v-tooltip
-              v-model="tooltip.enabled"
-              top
-              color="red"
-              v-if="loggedWallet.type === WalletType.Normal"
-            >
-              <template v-slot:activator="{ }">
+          <v-col cols="12" class="pt-6" style="display: flex; justify-content: center">
+            <v-tooltip v-model="tooltip.enabled" top color="red" v-if="loggedWallet.type === WalletType.Normal">
+              <template v-slot:activator="{}">
                 <v-text-field
                   flat
-                  style="max-width: 295px;"
+                  style="max-width: 295px"
                   block
                   dense
                   v-model="spendingPassword"
@@ -112,12 +148,28 @@
               </template>
               <span>{{ tooltip.text }}</span>
             </v-tooltip>
-            <div v-else-if="loggedWallet.type === WalletType.Ledger" class="py-0" style="align-content: center;">
+            <div v-else-if="loggedWallet.type === WalletType.Ledger" class="py-0" style="align-content: center">
               <v-card-subtitle class="pa-0 text-center justify-center pt-0" style="color: white">
-                <ToggleSwitch text-left="USB" icon-left="mdi-usb" text-right="Bluetooth" icon-right="mdi-bluetooth" v-model="isBT" :disabled="loading" />
+                <ToggleSwitch
+                  text-left="USB"
+                  icon-left="mdi-usb"
+                  text-right="Bluetooth"
+                  icon-right="mdi-bluetooth"
+                  v-model="isBT"
+                  :disabled="loading"
+                />
               </v-card-subtitle>
             </div>
-            <v-btn color="primary" elevation="0" @click="signAndSubmitDelegationTx" height="40" :disabled="loading || !valid" :loading="loading" class="mx-2" style="margin-bottom: 1px">
+            <v-btn
+              color="primary"
+              elevation="0"
+              @click="signAndSubmitDelegationTx"
+              height="40"
+              :disabled="loading || !valid"
+              :loading="loading"
+              class="mx-2"
+              style="margin-bottom: 1px"
+            >
               {{ isSubmit ? 'Submit' : 'Delegate' }}
             </v-btn>
           </v-col>
@@ -147,7 +199,7 @@ import filters from '@/shared/utils/filters';
 import CopyButton from '@/shared/components/CopyButton.vue';
 import { Cardano, Serialization } from '@cardano-sdk/core';
 import rules from '@/utils/rules';
-import networks from "@/utils/networks";
+import networks from '@/utils/networks';
 import snackbar from '@/plugins/snackbar';
 import { WalletType } from '@/models/types';
 // TODO: Keystone support needs to be reimplemented with Cardano JS SDK
@@ -174,7 +226,7 @@ const props = defineProps({
   tx: {
     type: Object as () => Cardano.Tx,
     default: () => {},
-  }
+  },
 });
 
 const emit = defineEmits(['close']);
@@ -214,11 +266,11 @@ const depositFee = computed(() => {
   if (!props.tx?.body) return 0;
 
   // Check for vote registration certificate with deposit
-  const registrationCert: Cardano.Certificate
-    = props.tx.body.certificates?.find(
-    (cert: any) => cert.__typename === Cardano.CertificateType.StakeVoteRegistrationDelegation ||
-            cert.__typename === Cardano.CertificateType.VoteRegistrationDelegation ||
-            cert.__typename === Cardano.CertificateType.VoteDelegation
+  const registrationCert: Cardano.Certificate = props.tx.body.certificates?.find(
+    (cert: any) =>
+      cert.__typename === Cardano.CertificateType.StakeVoteRegistrationDelegation ||
+      cert.__typename === Cardano.CertificateType.VoteRegistrationDelegation ||
+      cert.__typename === Cardano.CertificateType.VoteDelegation
   );
 
   // For DRep delegation, check if there's a deposit required
@@ -227,9 +279,7 @@ const depositFee = computed(() => {
   }
 
   // Also check for governance deposit in certificates
-  const drepRegistration = props.tx.body.certificates?.find(
-    (cert: any) => cert.deposit && (cert.dRep || cert.anchor)
-  );
+  const drepRegistration = props.tx.body.certificates?.find((cert: any) => cert.deposit && (cert.dRep || cert.anchor));
 
   if (drepRegistration && 'deposit' in drepRegistration && drepRegistration.deposit) {
     return Number(drepRegistration.deposit);
@@ -240,25 +290,25 @@ const depositFee = computed(() => {
 
 const cols = computed(() => {
   if (depositFee.value > 0) {
-    return 4
+    return 4;
   } else {
-    return 6
+    return 6;
   }
 });
 
 const getIconByURI = (uri: string) => {
   if (String(uri).includes('https://github.com')) {
-    return 'mdi-github'
+    return 'mdi-github';
   } else if (String(uri).includes('youtube.com') || String(uri).includes('youtu.be')) {
-    return 'mdi-youtube'
+    return 'mdi-youtube';
   } else if (String(uri).includes('linkedin.com')) {
-    return 'mdi-linkedin'
+    return 'mdi-linkedin';
   } else if (String(uri).includes('instagram.com')) {
-    return 'mdi-instagram'
+    return 'mdi-instagram';
   } else if (String(uri).includes('discord.com')) {
-    return 'mdi-discord'
+    return 'mdi-discord';
   }
-  return 'mdi-link'
+  return 'mdi-link';
 };
 
 // TODO: Keystone QR code functionality - currently disabled
@@ -273,16 +323,16 @@ const enableToolTip = () => {
 };
 
 const signTx = async (): Promise<boolean> => {
-  loading.value = true
+  loading.value = true;
   try {
     console.log('Signing Cardano JS SDK DRep delegation transaction');
     console.log('Transaction:', props.tx);
 
     // First, verify password via a background message
-    const passwordVerification = await Messaging.sendToBackgroundFromOptions({
+    const passwordVerification = (await Messaging.sendToBackgroundFromOptions({
       method: MessageTypes.VERIFY_SPENDING_PASSWORD,
-      data: { password: spendingPassword.value }
-    }) as { data: { isValid: boolean; error?: string } };
+      data: { password: spendingPassword.value },
+    })) as { data: { isValid: boolean; error?: string } };
 
     if (!passwordVerification.data.isValid) {
       enableToolTip();
@@ -295,7 +345,7 @@ const signTx = async (): Promise<boolean> => {
     console.log('Serialized transaction CBOR:', txCbor.value);
 
     // Sign the transaction via a background message
-    const witnessResult = await Messaging.sendToBackgroundFromOptions({
+    const witnessResult = (await Messaging.sendToBackgroundFromOptions({
       method: MessageTypes.SIGN_TX,
       data: {
         txCbor: txCbor.value, // Pass serialized CBOR instead of the object
@@ -305,8 +355,8 @@ const signTx = async (): Promise<boolean> => {
         utxos: utxos.value,
         addresses: keys.value, // Address mappings
         mergeWitnesses: false,
-      }
-    }) as { data: { witnesses?: any; error?: string } };
+      },
+    })) as { data: { witnesses?: any; error?: string } };
 
     console.log('Transaction signed successfully:', witnessResult);
 
@@ -319,10 +369,10 @@ const signTx = async (): Promise<boolean> => {
     return true;
   } catch (e) {
     console.error('Error signing DRep delegation transaction:', e);
-    snackbar.setError(e instanceof Error ? e.message : 'Unknown error')
+    snackbar.setError(e instanceof Error ? e.message : 'Unknown error');
     return false;
   } finally {
-    loading.value = false
+    loading.value = false;
   }
 };
 
@@ -338,16 +388,16 @@ const signLedgerTx = async () => {
       keys.value,
       utxos.value,
       !isBT.value, // isUsb flag (inverted from isBT)
-      networks.resolveNetwork(loggedWallet.value.chain, loggedWallet.value.network),
+      networks.resolveNetwork(loggedWallet.value.chain, loggedWallet.value.network)
     );
     const transactionWitnessSet: Serialization.TransactionWitnessSet = Serialization.TransactionWitnessSet.fromCore({
       signatures,
-    })
+    });
     console.log('[LEDGER-SIGN] signing successful:', transactionWitnessSet.toCbor());
     txWitnesses.value = transactionWitnessSet.toCbor();
     return true;
   } catch (e) {
-    ledgerUtils.ledgerErrorHandling(e)
+    ledgerUtils.ledgerErrorHandling(e);
     return false;
   } finally {
     loading.value = false;
@@ -356,28 +406,28 @@ const signLedgerTx = async () => {
 
 const submitTx = async () => {
   try {
-    loading.value = true
+    loading.value = true;
     console.log('Submitting Cardano JS SDK DRep delegation transaction');
-    const submitResult = await Messaging.sendToBackgroundFromOptions({
+    const submitResult = (await Messaging.sendToBackgroundFromOptions({
       method: MessageTypes.SUBMIT_TX,
       data: {
         txCbor: txCbor.value,
         witnessHex: txWitnesses.value,
-        utxos: utxos.value
-      }
-    }) as { data: { txId?: string; error?: string } };
+        utxos: utxos.value,
+      },
+    })) as { data: { txId?: string; error?: string } };
 
     if (submitResult.data.error) {
       throw new Error(submitResult.data.error);
     }
 
-    snackbar.fireSuccess(`DRep Delegation Tx Submitted Successfully. Tx ID: ${submitResult.data.txId}`)
-    emit('close')
+    snackbar.fireSuccess(`DRep Delegation Tx Submitted Successfully. Tx ID: ${submitResult.data.txId}`);
+    emit('close');
   } catch (e) {
     console.error('Error submitting DRep delegation transaction:', e);
-    snackbar.setError(e instanceof Error ? e.message : 'Unknown error')
+    snackbar.setError(e instanceof Error ? e.message : 'Unknown error');
   } finally {
-    loading.value = false
+    loading.value = false;
   }
 };
 
@@ -421,26 +471,25 @@ const signAndSubmitDelegationTx = async () => {
 };
 
 const fallbackImage = (e: any) => {
-  e.target.src = assets.errorImage
+  e.target.src = assets.errorImage;
 };
 
-watch(() => props.isOpen, (val) => {
-  if (val) {
-    spendingPassword.value = ''
-    showPassword.value = false
-    isSubmit.value = false
-    if (form.value) {
-      form.value.resetValidation()
+watch(
+  () => props.isOpen,
+  val => {
+    if (val) {
+      spendingPassword.value = '';
+      showPassword.value = false;
+      isSubmit.value = false;
+      if (form.value) {
+        form.value.resetValidation();
+      }
     }
   }
-});
+);
 
 watch(spendingPassword, () => {
-  passwordRules.value = [
-    rules.required()
-  ]
+  passwordRules.value = [rules.required()];
 });
 </script>
-<style scoped>
-
-</style>
+<style scoped></style>
